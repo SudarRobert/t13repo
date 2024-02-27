9f7b8daa354ce58fd57e7d3dad5acee0c77660e5
SudarRobert <sudirob6@gmail.com>
2024-02-27 18:57:10 +0100

    pici javítás JS-ben

--- 24_02_27_Nyomtatas/nyomtatas.js
+++ 24_02_27_Nyomtatas/nyomtatas.js
@@ -1,14 +1,3 @@
-/*function kalkulal(){
-    //Űrlapadatok
-    const szelesseg=297;
-    const magassag=420;
-    const papir=document.getElementById('papirtipus').value;
-    //Számítások
-    let terulet=Math.round((szelesseg*magassag)/10000);    
-    let koltseg=terulet*papir;
-
-*/
-
 function kalkulal() {
     // Űrlapadatok
     const szelesseg = document.getElementById('szelesseg').value;
