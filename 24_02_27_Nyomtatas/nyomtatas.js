d0d4a416cd1045902c8589007205af1311ce8cc4
SudarRobert <sudirob6@gmail.com>
2024-02-27 18:11:04 +0100

    nyomtatás feladatsor - Sudár Róbert T13

---
+++ 24_02_27_Nyomtatas/nyomtatas.js
@@ -0,0 +1,30 @@
+/*function kalkulal(){
+    //Űrlapadatok
+    const szelesseg=297;
+    const magassag=420;
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+    let terulet=Math.round((szelesseg*magassag)/10000);    
+    let koltseg=terulet*papir;
+
+*/
+
+function kalkulal() {
+    // Űrlapadatok
+    const szelesseg = document.getElementById('szelesseg').value;
+    const magassag = document.getElementById('magassag').value;
+    const papir = document.getElementById('papirtipus').value;
+
+    // Számítások
+    let terulet = Math.round((szelesseg * magassag) / 10000);
+    let koltseg = terulet * papir;
+
+    document.getElementById('terulet').textContent = terulet;
+    document.getElementById('papir').textContent = papir;
+
+    // Költség megjelenítése
+    document.getElementById('koltseg').textContent = koltseg;
+
+    // Keret láthatóvá tétele
+    document.getElementById('valasz').style.visibility = "visible";
+}
